77f51f7529d9bb4438415196d2e2afc719bf8d7b
Sindre Moen <sindrenm@gmail.com>
2021-06-01 22:35:29 +0200

    Work around test bug in AGP 7.0.0-beta03
    There's a bug in AGP 7.0.0-beta03 where empty test directories will
    cause `./gradlew test` to fail. So here are some empty tests to work
    around that issue.
    
    See https://issuetracker.google.com/issues/189559355 for details.

---
+++ app/src/test/java/dev/sindrenm/template/app/EmptyTest.kt
@@ -0,0 +1,11 @@
+package dev.sindrenm.template.app
+
+import org.junit.jupiter.api.Test
+
+// TODO: Remove after upgrading from AGP 7.0.0-beta03
+//  See https://issuetracker.google.com/issues/189559355.
+class EmptyTest {
+  @Test
+  fun testNothing() {
+  }
+}
